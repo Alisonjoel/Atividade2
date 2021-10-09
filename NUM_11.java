57ddace495daf55137fe9f9808e2038a04044c17
Alisonjoel <92141102+Alisonjoel@users.noreply.github.com>
2021-10-09 00:10:33 -0300

    Update NUM_11.java

--- NUM_11.java
+++ NUM_11.java
@@ -1,9 +1,25 @@
-
+import java.util.Scanner;
 public class NUM_11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
 
+		System.out.print("Que turno você estuda? (M) = Matutino ; (V) Vespertino ; (N) Noturno : ");
+		String letra = entrada.nextLine();
+
+		if ((letra.equals("m"))) {
+			System.out.println("Bom dia");
+		}
+		else {
+			
+			if ((letra.equals("v"))) { 
+				System.out.print("Boa tarde");
+			}
+			else {			
+			System.out.println ("Boa noite");
+		    }
+		}
+		entrada.close();
 	}
-
 }
