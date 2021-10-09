0d9946f5c8c5a56ce7d2bfab0a6eae2cf5d389dc
Alisonjoel <92141102+Alisonjoel@users.noreply.github.com>
2021-10-09 00:14:23 -0300

    Update NUM_14.java

--- NUM_14.java
+++ NUM_14.java
@@ -1,9 +1,40 @@
- 	
+import java.util.Scanner;
 public class NUM_14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("informe um dia da semana : " + "|Domingo (1)" + "|Segunda (2)"
+		+ "|Terça (3)"+"|Quarta (4)"+"|Quinta (5)"+"|Sexta (6)"+"|Sábado (7)");
+		int dia = entrada.nextInt();
+		
+		if (dia ==1) {
+		System.out.println("Domingo");
+		}
+		else if (dia ==2) { 
+		System.out.println("Segunda");
+		}
+		else if (dia ==3) {
+		System.out.println("Terça");
+		}
+		else if (dia ==4) {
+		System.out.println("Quarta");
+		}
+		else if (dia ==5) {
+		System.out.println("Quinta");
+		}
+		else if (dia ==6) {
+		System.out.println("Sexta");
+		}
+		else if (dia ==7) {
+		System.out.println("Sábado");
+	    }
+		else {
+			System.out.print("Valor inválido");
+		}
+		
+		entrada.close();	
 	}
-
 }
+	
