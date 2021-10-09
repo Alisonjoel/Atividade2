cc813f7d5df39b390438afb906a54e18b272e462
Alisonjoel <92141102+Alisonjoel@users.noreply.github.com>
2021-10-09 00:24:55 -0300

    Update NUM_25.java

--- NUM_25.java
+++ NUM_25.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
-public class NUM_25 {
+public class NUM_25_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		try (// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in)) {
+			
+			System.out.print("Informe um numero : ");
+			float num = entrada.nextFloat();
+			
+			if (num % 2 == 0) {
+				System.out.print("O número é par");
+			}
+			else {
+				System.out.print("O número é impar");
+			}
+			
+			entrada.close();
+		 }
+	  }
+   }
