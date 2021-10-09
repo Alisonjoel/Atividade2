4a63c6de383fda90a74008c1d1e94f210a8f7dae
Alisonjoel <92141102+Alisonjoel@users.noreply.github.com>
2021-10-09 00:15:26 -0300

    Update NUM_15.java

--- NUM_15.java
+++ NUM_15.java
@@ -1,9 +1,51 @@
+import java.util.Scanner;
+//14
+public class NUM_15
+{
 
-public class NUM_15 {
-
-	public static void main(String[] args) {
+	public static void main (String[]args)
+	{
 		// TODO Auto-generated method stub
 
-	}
+		Scanner entrada = new Scanner (System.in);
 
+		System.out.print ("informe codigo do produto : ");
+		int codigo = entrada.nextInt ();
+
+		System.out.print ("informe preco de custo do produto : ");
+		int valor = entrada.nextInt ();
+
+		if (codigo == 1){
+
+			System.out.println ("Origem sul");
+			System.out.println ("preco :" + valor);	
+		
+		if (codigo == 2) 
+			System.out.println ("Origem sul");
+			System.out.println ("preco :" + valor);
+		
+
+		if (codigo == 3) 
+			System.out.println ("Origem Leste");
+			System.out.println ("preco :" + valor);
+		
+		if (codigo == 4) 
+			System.out.println ("Origem Oeste");
+			System.out.println ("preco :" + valor);
+		
+
+		if ((codigo == 5)|(codigo == 6)) 
+			System.out.println ("Origem nordeste");
+			System.out.println ("preco :" + valor);
+		
+		if ((codigo == 7)|(codigo == 8)) 
+			System.out.println ("Origem Centro-oeste");
+			System.out.println ("preco :" + valor);
+		}
+		
+		else {
+			System.out.println ("Produto importado");
+		}
+		entrada.close ();
+	}
 }
