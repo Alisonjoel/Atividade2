ac7396e03a45659bd501fbcfe89edea4686a8b84
Alisonjoel <92141102+Alisonjoel@users.noreply.github.com>
2021-10-09 00:07:42 -0300

    Update NUM8.java

--- NUM8.java
+++ NUM8.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class NUM8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("informe a primeira nota : ");
+		int numI = entrada.nextInt();
+		System.out.print("informe a segunda nota : ");
+		int numII = entrada.nextInt();
+		
+		if ((numII+numII)/2 >= 6) {
+			System.out.println ("Você foi aprovado");
+		}
+		else{
+			System.out.println ("Você foi reprovado");
+		}
+		
+		entrada.close();
 	}
-
 }
