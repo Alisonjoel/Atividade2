c5069486ade8dfc17c17ab56bfa5ac25a3c75e78
Alisonjoel <92141102+Alisonjoel@users.noreply.github.com>
2021-10-09 00:22:33 -0300

    Update NUM_23.java

--- NUM_23.java
+++ NUM_23.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class NUM_23 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Informe a idade do primeiro aluno: ");
+		int numI = entrada.nextInt();
+		
+		System.out.print("Informe a idade do segundo aluno: ");
+		int numII = entrada.nextInt();
+		
+		System.out.print("Informe a idade do terceiro aluno: ");
+		int numIII = entrada.nextInt();
+		
+		float media = (float) ((numI + numII + numIII) / 3);
+
+		if (media < 25) {
+			System.out.println("Turma Jovem");
+		}
+		else if ((media > 25) && (media <= 40)) {
+			System.out.println("Turma Adulta");
+		}
+		else {
+			System.out.println("Turma Idosa");
+		}
+		entrada.close();
 	}
-
 }
