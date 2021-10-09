fe1bf951e4da85ce868155482ecb1b74dedbbba6
Alisonjoel <92141102+Alisonjoel@users.noreply.github.com>
2021-10-09 00:05:34 -0300

    Update NUM6.java

--- NUM6.java
+++ NUM6.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
-public class NUM6 {
+public class NUM6{
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite uma numero : ");
+		int numI= entrada.nextInt();
+				if ((numI % 2) == 0) {
+			System.out.println ("O numero digitado é par");
+		}
+		else{
+			System.out.println("O numero digitado é impar");
+		}
+		entrada.close();
+	}
 }
